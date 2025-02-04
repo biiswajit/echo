2b184dc1ab521470450f3dbb76bea0411a29b991
Biswajit Malakar <mebiswajitmalakar@gmail.com>
2025-02-04 23:34:05 +0530

    Figure out base features for models
    Create a simple base class in FastAPI
    Base class contains all basic features need for each models
    These features are not fixed, may change in future

--- apps/api/src/main.py
+++ apps/api/src/main.py
@@ -1,7 +1,17 @@
+import os
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv() # load environment variables from .env
 
 app = FastAPI()
 
 @app.get("/greet/{name}")
 async def greet(name):
+    print(os.environ.get("API_KEY"))
     return {"message": f"hello, {name}"}
+
+@app.get("/")
+async def main():
+    print(os.environ.get("API_KEY"))
+    return "Hello, there!"
---
+++ apps/api/src/models/base.py
@@ -0,0 +1,24 @@
+class Model:
+    def __init__(self):
+        pass
+
+    def generate_title(self, prompt):
+        pass
+
+    def start_discussion(self, prompt):
+        pass
+
+    def respond(self, prompt):
+        pass
+
+    def iterate_response(self, response, prompt):
+        pass
+
+    def reply(self, response, snippet, message):
+        pass
+
+    def get_history(self):
+        pass
+
+    def get_discussions(self):
+        pass
