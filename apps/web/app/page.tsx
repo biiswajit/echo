72f516161cfcf9850e29839985a1c4de160b09db
Biswajit Malakar <mebiswajitmalakar@gmail.com>
2025-02-17 14:00:34 +0530

    Init Prisma
    Create packages/database package for prisma
    Import packages/database in apps/web project
    Import packages/database in apps/api project
    Import packages/database in apps/mapper project

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,9 @@
-import Button from "@echo/ui/button";
-
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-      <Button />
     </div>
   )
 }
